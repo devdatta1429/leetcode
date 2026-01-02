bf402867f03549c58231aec5db4d2933a57d582c
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-01-02 23:42:48 +0530

    Update longest-substring-without-repeating-characters.py

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,7 +8,6 @@ class Solution:
             while string1[right] in seen:
                 seen.remove(string1[left])
                 left += 1
-
             seen.add(string1[right])
             max_len = max(max_len, right - left + 1)
 
