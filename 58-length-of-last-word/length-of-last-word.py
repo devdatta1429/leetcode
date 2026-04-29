f581de90c5cb1ea24e98b7f22addb73409a276e6
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-04-30 00:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.72%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -4,9 +4,3 @@ class Solution:
         if type(s)==str: 
             return len(s) 
 
-    
-#    class Solution:
-#    def lengthOfLastWord(self, s: str) -> int:
-#        s = s.rstrip()  # s = "   fly me   to   the moon" only remove trailing spaces
-#        return len(s) - s.rfind(' ') - 1
-#                 25   -  22(where we find the space) -1 
