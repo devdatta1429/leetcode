8b8607d990a0ffde9bb98aa06677f09cff28474b
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-05-03 22:23:13 +0530

    Time: 304 ms (33.40%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,7 +1,3 @@
-with big_coun as(
-    select name, population,area
-    from World
-    where area >= 3000000 or population >= 25000000
-)
-select * 
-from big_coun;
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
