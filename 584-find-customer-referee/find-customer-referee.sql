5adfe396b7117ae0fb02141513c595d09592bf90
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-05-03 22:01:58 +0530

    Time: 472 ms (54.44%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,8 +1,4 @@
-with ref_id_2 as(
-    select name
-    from Customer
-    where referee_id != 2 or referee_id is null
-)
 
-select * 
-from ref_id_2;
+select name
+from Customer
+where referee_id != 2 or referee_id is null;
