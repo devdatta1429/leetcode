59dc2b1cf71ef7b5766966d32fd4fbee6e74a313
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-06-03 15:58:30 +0530

    Time: 4 ms (39.37%) | Memory: 20.4 MB (42.47%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, nums, target):
-        dic= {}
-        for i in range(len(nums)):
-            dic[nums[i]]= i
-        
-        for i in range(len(nums)):
-            remain = target - nums[i]
-            if remain in dic.keys() and i != dic[remain]:
-                return dic[remain],i  
+        dic = {}
+
+        for i , num in enumerate(nums):
+            remain = target - num
+            if remain in dic:
+                return dic[remain],i
+            
+            dic[num] = i  
