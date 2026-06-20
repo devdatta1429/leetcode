8991c036e614b42373c40b53a9483f085b8f7255
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-06-20 23:50:26 +0530

    Time: 1390 ms (75.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,5 @@
+select name
+from SalesPerson
+where sales_id not in(select sales_id
+                        from Company c join Orders o on c.com_id = o.com_id
+                        where c.name = 'RED');
