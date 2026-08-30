c286df94efda2f640275198977e48ed2f3abe8a0
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-08-30 15:42:04 +0530

    Time: 3 ms (86.47%) | Memory: 13.8 MB (14.19%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        candidate=None
+        count=0
+        for i in nums:
+            if count==0:
+                candidate=i
+            if i==candidate:
+                count+=1
+            else:
+                count-=1
+        return candidate
+        
