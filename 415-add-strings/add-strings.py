3ce355d81c22ef614b0555b7966f20f3ff260803
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-09-03 10:41:18 +0530

    Time: 12 ms (22.79%) | Memory: 12.6 MB (25.58%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -3,20 +3,20 @@ class Solution(object):
         nums=[]
         carry=0
 
-        len1=len(num1)-1
-        len2=len(num2)-1
+        i=len(num1)-1
+        j=len(num2)-1
 
-        while len1>=0 or len2>=0 or carry:
+        while i>=0 or j>=0 or carry>0:
 
-            if len1>=0:
-                carry+=int(num1[len1])
-                len1-=1
+            if i>=0:
+                carry+=int(num1[i])
+                i-=1
 
-            if len2>=0:
-                carry+=int(num2[len2])
-                len2-=1
+            if j>=0:
+                carry+=int(num2[j])
+                j-=1
 
             nums.append(str(carry%10))
             carry= carry //10
 
-        return str(''.join(nums[::-1]))
+        return ''.join(nums[::-1])
