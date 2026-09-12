47914b794aa5d0af775f6fe071d148c272794f01
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-09-12 21:39:57 +0530

    Time: 3 ms (38.37%) | Memory: 12.3 MB (97.79%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,23 +1,23 @@
 class Solution(object):
     def isSubsequence(self, s, t):
-        # k = ""
-        # z=len(s)+1
-        # r=s[-z:]
-        # for i in t:
-        #     if z>0:
-        #         if i in s:
-        #             k+=i
-        #             z-=1
-        # if k==s:
-        #     return True
-        # else:
-        #     return False
+        k = ""
+        z = 0
+        
+        for i in t:
+            if z < len(s):
+                if i==s[z]:
+                    k+=i
+                    z+=1
+        if k==s:
+            return True
+        else:
+            return False
 
    
-        j = 0
+        # j = 0
 
-        for i in t:
-            if j < len(s) and i == s[j]:
-                j += 1
+        # for i in t:
+        #     if j < len(s) and i == s[j]:
+        #         j += 1
 
-        return j == len(s)
+        # return j == len(s)
