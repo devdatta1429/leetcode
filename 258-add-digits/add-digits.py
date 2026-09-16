65c59334e3ead5ad25dccd896a5ee03b7a17893e
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-09-16 18:40:16 +0530

    Time: 7 ms (5.37%) | Memory: 12.2 MB (99.39%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addDigits(self, num):
+        while num>=10:
+            total=0
+            for i in str(num):
+                total+=(int(i))
+            num=total
+        return num
+        
