5968eea44ae11d1da433fcc6fbd14db7380fb0c5
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-09-20 23:59:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.93%) - LeetSync

---
+++ 504-base-7/base-7.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def convertToBase7(self, num):
+        if num == 0:
+            return "0"
+
+        sign = "-" if num < 0 else ""
+        num = abs(num)
+
+        digits = []
+
+        while num > 0:
+            digits.append(str(num % 7))
+            num //= 7
+
+        return sign + "".join(reversed(digits))
